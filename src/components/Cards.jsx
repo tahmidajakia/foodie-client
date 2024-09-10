60eb4a667b268f04c31eed2f6aa26a74444171ea
Mst Tahmida Begum Jakia <devtahmidajakia@gmail.com>
2024-09-10 05:51:10 -0700

    created a cards section on home page

---
+++ src/components/Cards.jsx
@@ -0,0 +1,44 @@
+/* eslint-disable react/prop-types */
+
+import { useState } from "react";
+import { FaHeart } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const Cards = ({ item }) => {
+    const [isHeartFilled, setIsHeartFilled] = useState(false);
+
+  const handleHeartClick = () => {
+    setIsHeartFilled(!isHeartFilled);
+  };
+  return (
+    <div>
+      <div  className="card shadow-xl relative mr-5 md:my-5">
+      <div
+        className={`rating gap-1 absolute right-2 top-2 p-4 heartStar bg-green ${
+          isHeartFilled ? "text-rose-500" : "text-white"
+        }`}
+        onClick={handleHeartClick}
+      >
+        <FaHeart className="w-5 h-5 cursor-pointer" />
+      </div>
+      <Link to={`/menu/${item._id}`}>
+        <figure>
+          <img src={item.image} alt="Shoes" className="hover:scale-105 transition-all duration-300 md:h-72" />
+        </figure>
+      </Link>
+        <div className="card-body">
+          <h2 className="card-title">{item.name}</h2>
+          <p>Description Of The Item</p>
+          <div className="card-actions justify-end">
+          <h5 className="font-semibold">
+            <span className="text-sm text-red">$ </span> {item.price}
+          </h5>
+          <button className="btn bg-green text-white">Add to Cart </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
