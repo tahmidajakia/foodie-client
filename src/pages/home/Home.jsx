f8bc992f3dc043e5c305a5cabc6ee34c3e2e0703
Mst Tahmida Begum Jakia <devtahmidajakia@gmail.com>
2024-09-10 02:54:04 -0700

    created a Popular Catagories section on home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,14 @@
 import Banner from "../../components/Banner";
+import Categories from "./Categories";
+import SpecialDishes from "./SpecialDishes";
 
 
 const Home = () => {
     return (
         <div>
            <Banner></Banner>
+           <Categories></Categories>
+           <SpecialDishes></SpecialDishes>
         </div>
     );
 };
---
+++ src/pages/home/SpecialDishes.jsx
@@ -0,0 +1,95 @@
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import React, { useEffect, useState } from "react";
+
+
+const SpecialDishes = () => {
+  const [recipes, setRecipes] = useState([]);
+  const slider = React.useRef(null);
+
+  useEffect(() => {
+    fetch("/menu.json")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data)
+      // console.log(specials)
+      
+    });
+  },[])
+
+  const settings = {
+    dots: true,
+    infinite: true, // Changed to true for continuous scrolling
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true, // Ensures responsiveness continues
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 0, // Adjusted to show the first slide
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  return (
+    <div className="max-w-screen-2xl container mx-auto xl:px-24 px-4 my-20">
+      <div className="text-left mb-8">
+        <p className="subtitle">Customer Favorites</p>
+        <h2 className="title">Popular Categories</h2>
+      </div>
+
+      <div className="slider-container">
+      <Slider {...settings}>
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+        <div>
+          <h3>5</h3>
+        </div>
+        <div>
+          <h3>6</h3>
+        </div>
+        <div>
+          <h3>7</h3>
+        </div>
+        <div>
+          <h3>8</h3>
+        </div>
+      </Slider>
+    </div>
+    </div>
+  );
+};
+
+export default SpecialDishes;
